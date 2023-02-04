97d08e8d72aacfb717b66ab2686e59206c582d7a
ghani <skghani9999@gmail.com>
2023-02-04 14:03:38 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- bootstrap4 -->
+        <link rel="stylesheet" href="css\bootstrap.min.css">
+    <!-- font awsome-->
+    <link rel="stylesheet" href="css\all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=PT+Serif&family=Poppins&family=Roboto:ital,wght@0,300;1,300&family=Ubuntu:ital,wght@0,500;1,500&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="css\style.css">
+   
+    <title>LuvBAse | home</title>
+
+
+</head>
+<body>
+    <!--navigation starts here -->
+<nav class="navbar navbar-expand-sm   navbar-dark  pl-5 fixed-top">
+  <a class="navbar-brand" href="index.php">LuvBase</a>
+  <span class="nav-text text-white">Learn with us </span>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <ul class="navbar-nav cust-nav pl-5">
+        <li class="nav-item cust-nav-item"><a class="nav-link" href="#">Home</a></li>
+        <li class="nav-item cust-nav-item"><a class="nav-link" href="#">Courses</a></li>
+        <li class="nav-item cust-nav-item"><a class="nav-link" href="#">Profile</a></li>
+        <li class="nav-item cust-nav-item"><a class="nav-link" href="#" data-toggle="modal" data-target="#logModal">Login</a></li>
+        <li class="nav-item cust-nav-item"><a class="nav-link" href="#">Log Out</a></li>
+
+        <li class="nav-item"><a class="nav-link" href="#">Sign up</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+      
+</ul>
+  </div>
+</nav>
+  <!--navigation Ends here here -->
+  <!-- Video background -->
+  <div  class="container-fluid rem-vid-marg">
+    <div class="vidparent">
+        <video playsinline autoplay muted loop >
+            <source src="image/banvid1.mp4" >
+        </video>
+       <div class="vid-overlay"></div>
+        <div class="vid-content">
+    <h1 class="my-content"> Welcome to Luv Base</h1>
+    <small  class="my-content">Learn and Implement</small>
+    <br>
+    <a href="#" class="btn btn-danger"  data-toggle="modal" data-target="#regModal">Get Started</a>
+    </div>
+    </div>
+   </div>
+     <!-- Video background  ends-->
+       <!-- start text banner -->
+       <div class="container-fluid bg-danger txt-baner">
+        <div class="row bottom-baner" >
+        <div class="col-sm"><h5><i class="fas fa-book-open mr-3"></i>100+ Online courses</h5></div>
+        <div class="col-sm"><h5><i class="fas fa-users mr-3"></i>Expert Instructors</h5></div> 
+        <div class="col-sm"><h5><i class="fas fa-keyboard mr-3"></i>Life time Access</h5></div>
+
+        </div>  
+
+       </div>
+          <!-- ends text banner -->
+          <!-- starts  popular courses desk -->
+   <div class="container mt-5">
+    <h1 class="text-center">popular courses</h1>
+
+    <!-- starts   courses desk 1-->
+    <div class="card-deck mt-4">
+        <a href="#" class="btn" style="text-align:left;padding:0px;">
+    
+        <div class="card ">
+        <img  src="image\img-1 (1).jpg" class="card-img-top"alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text</p>
+  </div>
+  <div class="card-footer" >
+    <p class="card-text d-inline">Price:<small><del>&#8377 200</del></small>
+<span class="font-weight-bolder">&#8377 200</span><a href=""></p>
+<a class="btn btn-primary text-white font-weight-bolder">Enroll</a>
+    
+  </div>
+</div>
+    
+    
+    </a>
+    <a href="#" class="btn" style="text-align:left;padding:0px;">
+    
+    <div class="card ">
+    <img class="card-img-top" src="image\img-1 (2).jpg" alt="Card image cap">
+<div class="card-body">
+<h5 class="card-title">Special title treatment</h5>
+<p class="card-text">With supporting text</p>
+</div>
+<div class="card-footer" >
+<p class="card-text d-inline">Price:<small><del>&#8377 200</del></small>
+<span class="font-weight-bolder">&#8377 200</span><a href=""></p>
+<a class="btn btn-primary text-white font-weight-bolder">Enroll</a>
+
+</div>
+</div>
+
+
+</a>
+<a href="#" class="btn" style="text-align:left;padding:0px;">
+    
+    <div class="card">
+    <img class="card-img-top" src="image\img-1 (3).jpg" alt="Card image cap">
+<div class="card-body">
+<h5 class="card-title">Special title treatment</h5>
+<p class="card-text">With supporting text.</p>
+</div>
+<div class="card-footer" >
+<p class="card-text d-inline">Price:<small><del>&#8377 200</del></small>
+<span class="font-weight-bolder">&#8377 200</span><a href=""></p>
+<a class="btn btn-primary text-white font-weight-bolder">Enroll</a>
+
+</div>
+</div>
+
+
+</a>
+
+    </div>
+     <!-- ends  courses desk 1-->
+        <!-- starts   courses desk 2-->
+    <div class="card-deck mt-4">
+    <a href="#" class="btn" style="text-align:left;padding:0px;">
+    
+        <div class="card ">
+        <img class="card-img-top" src="image\img-1 (4).jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text .</p>
+  </div>
+  <div class="card-footer" >
+    <p class="card-text d-inline">Price:<small><del>&#8377 200</del></small>
+<span class="font-weight-bolder">&#8377 200</span><a href=""></p>
+<a class="btn btn-primary text-white font-weight-bolder">Enroll</a>
+    
+  </div>
+</div>
+    
+    
+    </a>
+    <a href="#" class="btn" style="text-align:left;padding:0px;">
+    
+    <div class="card ">
+    <img class="card-img-top" src="image\img-1 (2).jpg" alt="Card image cap">
+<div class="card-body">
+<h5 class="card-title">Special title treatment</h5>
+<p class="card-text">With supporting text .</p>
+</div>
+<div class="card-footer" >
+<p class="card-text d-inline">Price:<small><del>&#8377 200</del></small>
+<span class="font-weight-bolder">&#8377 200</span><a href=""></p>
+<a class="btn btn-primary text-white font-weight-bolder">Enroll</a>
+
+</div>
+</div>
+
+
+</a>
+<a href="#" class="btn" style="text-align:left;padding:0px;">
+    
+    <div class="card">
+    <img class="card-img-top" src="image\img-1 (3).jpg" alt="Card image cap">
+<div class="card-body">
+<h5 class="card-title">Special title treatment</h5>
+<p class="card-text">With supporting text .</p>
+</div>
+<div class="card-footer" >
+<p class="card-text d-inline">Price:<small><del>&#8377 200</del></small>
+<span class="font-weight-bolder">&#8377 200</span><a href=""></p>
+<a class="btn btn-primary text-white font-weight-bolder">Enroll</a>
+
+</div>
+</div>
+
+
+</a>
+
+    </div>
+     <!-- ends  courses desk 2-->
+   </div>
+
+
+          <!-- ends popular courses deck -->
+
+<!-- contact us section starts from here -->
+<div class="container mt-4">
+    <h2 class="text-center mb-4">Contact us</h2>
+    <div class="row">
+        <div class="col-md-8">
+               <form action="" method="post">
+                <input type="text"  class="form-control" name="" placeholder="Enter Name " /><br>
+                <input type="text"  class="form-control" name="" placeholder="Subject" /><br>
+                <input type="email"  class="form-control" name="" placeholder="Enter Email" /><br>
+                <textarea   class="form-control" name="" placeholder="How can we help you" style="height:150px"></textarea><br>
+                <input class="btn btn-primary" type="submit" name="" value="send" name="submit" /><br><br>
+               </form>
+        </div>
+   <!-- contact us section starts from here -->
+
+   <!-- adress section starts from here -->
+
+        <div class="col-md-4 stripe text-white text-center">
+            <h4>Luv Base</h4>
+            <p>Luv base,
+                Hanamkonda<br>
+                warangal<br>
+                hyderabad
+            </p>
+              
+        </div>
+        <!-- adress section starts from here -->
+    </div>
+</div>
+
+
+<!-- contact us section ends-->
+<!-- footer--->
+
+<!-- footer-->
+
+
+<!-- student registration form starts from here -->
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="regModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Sign Up</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form>
+      <div class="form-group">
+   <i class="fas fa-user"></i> <label for="userName " class="pl-2 font-weight-bold">Name</label>
+  <input type="text" class="form-control" name="stuname" id="exampleInputPassword1" placeholder="Password">
+  </div>
+ 
+  <div class="form-group">
+  <i class="fas fa-envelope"></i> <label for="userEmail1" class="pl-2 font-weight-bold">Email address</label>
+    <input type="email" class="form-control" name="stuEmail" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+  <i class="fas fa-key"></i> <label for="userPassword" class="pl-2 font-weight-bold">Password</label>
+    <input type="password" class="form-control" name="stupass" id="exampleInputPassword1" placeholder="Password">
+  </div>
+ 
+</form>
+      </div>
+      <div class="modal-footer">
+      <button type="button" class="btn btn-primary">Sighn Up</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- student registration form ends from here -->
+
+  <!-- student login form starts from here -->
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="logModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Login here</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form>
+  
+ 
+  <div class="form-group">
+  <i class="fas fa-envelope"></i> <label for="Email1" class="pl-2 font-weight-bold">Email address</label>
+    <input type="email" class="form-control" name="uerEmail" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+  <i class="fas fa-key"></i> <label for="Password" class="pl-2 font-weight-bold">Password</label>
+    <input type="password" class="form-control" name="userPass" id="InputPassword1" placeholder="Password">
+  </div>
+ 
+</form>
+      </div>
+      <div class="modal-footer">
+      <button type="button" class="btn btn-primary">Login</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- student login form ends from here -->
+
+
+ <!-- jquery-->
+<script src="js\jquery.min.js" ></script>
+   <!-- bootstrap4 -->
+<script src="js\propper.min.js" ></script>
+ 
+<script src="js\bootstrap.min.js" ></script>
+  <!-- font awsome-->
+<script src="js\all.min.js" ></script>
+</body>
+</html>
